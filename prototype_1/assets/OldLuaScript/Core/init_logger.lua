43689796cc76b91df5282e4641f8008216048d7a
Michael Wang <michael.icwang@gmail.com>
2014-01-26 15:19:21 +0800

    Clean up.

--- prototype_1/assets/OldLuaScript/Core/init_logger.lua
+++
@@ -1,4 +0,0 @@
--- Initialize logger before running Core.lua, so we can get error message when something goes wrong.
-g_Logger = LuaLogger();
-g_JavaInterface = JavaInterface();
-g_Logger:Create(g_JavaInterface:GetLogFilePath());
